14d33d099c829896d5bd92e8a8d2f662059946da
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-28 14:20:04 -0500

    /workspace/github/nelsonmarro/kyber-med

--- internal/user/migrations/user_migrate.go
+++ internal/user/migrations/user_migrate.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"errors"
 	"math/rand"
 	"os"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2/log"
 	"golang.org/x/crypto/bcrypt"
-	"gorm.io/gorm"
 
 	"github.com/nelsonmarro/kyber-med/config"
 	"github.com/nelsonmarro/kyber-med/internal/database"
@@ -22,10 +20,18 @@ func main() {
 	conf := config.LoadConfig("config")
 	db := database.NewDatabase(conf)
 
-	pacientMigrate(db)
+	if len(args) > 0 && args[0] == "--migrate" {
+		migrateTables(db)
+	} else if len(args) > 0 && args[0] == "--seed" {
+		pacientSeed(db)
+	}
 }
 
-func pacientMigrate(db database.Database) {
+func migrateTables(db database.Database) {
+	db.GetDb().AutoMigrate(&entities.User{})
+}
+
+func pacientSeed(db database.Database) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte("123456"), bcrypt.DefaultCost)
 	if err != nil {
 		log.Error("error encryptar password")
@@ -48,17 +54,8 @@ func pacientMigrate(db database.Database) {
 		Password: string(hashedPassword),
 	}
 
-	db.GetDb().AutoMigrate(&entities.User{})
-
-	var userDb entities.User
-	result := db.GetDb().First(&userDb)
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		db.GetDb().Create(&user)
-		db.GetDb().Create(&admin)
-	} else {
-		db.GetDb().Migrator().DropTable(entities.User{})
-		db.GetDb().Migrator().CreateTable(entities.User{})
-		db.GetDb().Create(&user)
-		db.GetDb().Create(&admin)
-	}
+	db.GetDb().Migrator().DropTable(entities.User{})
+	db.GetDb().Migrator().CreateTable(entities.User{})
+	db.GetDb().Create(&user)
+	db.GetDb().Create(&admin)
 }
