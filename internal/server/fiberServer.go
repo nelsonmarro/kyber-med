b792e0639a06b00a18b3fa70234dfa1c0058804a
Nelson Marro <nelsonmarro99@gmail.com>
2025-02-03 21:30:10 -0500

    /workspace/github/nelsonmarro/kyber-med

--- internal/server/fiberServer.go
+++ internal/server/fiberServer.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -43,8 +45,16 @@ func (s *fiberServer) Start() {
 
 	s.app.Use(healthcheck.New())
 
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://gofiber.io, https://gofiber.net",
+	s.app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:8100",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodHead,
+			fiber.MethodPut,
+			fiber.MethodDelete,
+			fiber.MethodPatch,
+		}, ","),
 	}))
 
 	jwt := middlewares.NewJwtMiddleware(s.conf.Jwt.Key)
