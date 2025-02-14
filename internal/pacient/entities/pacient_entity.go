cf86f0b6b012494d901f895c2ad1e78a15935815
Nelson Marro <nelsonmarro99@gmail.com>
2025-02-14 15:30:04 -0500

    /workspace/github/nelsonmarro/kyber-med

--- internal/pacient/entities/pacient_entity.go
+++ internal/pacient/entities/pacient_entity.go
@@ -9,25 +9,24 @@ import (
 
 type Pacient struct {
 	commonentities.BaseEntity
+	UserID      string         `gorm:"type:uuid;not null"`
+	User        uEntities.User `gorm:"foreignKey:UserID;references:ID"`
 	FirstName   string         `gorm:"type:varchar(100);not null"`
 	LastName    string         `gorm:"type:varchar(100);not null"`
 	Email       string         `gorm:"type:varchar(100);not null;unique"`
 	IDCard      string         `gorm:"type:varchar(15);not null;unique"`
 	PhoneNumber string         `gorm:"type:varchar(20)"`
 	DateOfBirth time.Time      `gorm:"type:date;not null"`
-	Gender      string         `gorm:"type:varchar(10)"`
 	Address     string         `gorm:"type:varchar(350)"`
-	UserID      string         `gorm:"type:uuid;not null"`
-	User        uEntities.User `gorm:"foreignKey:UserID;references:ID"`
 	// Datos básicos
-	Edad   uint    `gorm:"not null"`
-	Genero string  `gorm:"type:varchar(50);not null"`
-	Altura float64 `gorm:"not null"`
-	Peso   float64 `gorm:"not null"`
+	Age    uint    `gorm:"not null"`
+	Gender string  `gorm:"type:varchar(50);not null"`
+	Height float64 `gorm:"not null"`
+	Weight float64 `gorm:"not null"`
 
 	// Datos de Metas / Objetivos
-	PesoObjetivo      float64    `gorm:"not null"`                   // kg
-	NivelActividad    string     `gorm:"type:varchar(100);not null"` // p.ej. "Sedentario", "Ligero", "Moderado", etc.
-	ObjetivoDietetico string     `gorm:"type:varchar(100);not null"` // p.ej. "Perder peso", "Salud mejorada", etc.
-	FechaMeta         *time.Time `gorm:"type:date"`
+	TargetWeight  float64    `gorm:"not null"`                   // kg
+	ActivityLevel string     `gorm:"type:varchar(100);not null"` // p.ej. "Sedentario", "Ligero", "Moderado", etc.
+	DietaryGoal   string     `gorm:"type:varchar(100);not null"` // p.ej. "Perder peso", "Salud mejorada", etc.
+	TargetDate    *time.Time `gorm:"type:date"`
 }
