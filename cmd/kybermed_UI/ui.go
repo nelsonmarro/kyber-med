eba9bc27b7b763f9172de46a214616cbb51653fc
Nelson Marro <nelsonmarro99@gmail.com>
2025-02-22 12:50:04 -0500

    /workspace/github/nelsonmarro/kyber-med

--- cmd/kybermed_UI/ui.go
+++ cmd/kybermed_UI/ui.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	mainWindow := a.NewWindow("Hello World")
+	mainWindow := a.NewWindow("Login")
 
-	mainWindow.SetContent(widget.NewLabel("Hello Wolrd!"))
+	mainWindow.SetContent()
 	mainWindow.ShowAndRun()
 }
