7c903e00e1624a3187227f4f01e6dda4545f8a69
Nelson Marro <nelsonmarro99@gmail.com>
2025-02-23 08:30:03 -0500

    /workspace/github/nelsonmarro/kyber-med

--- cmd/kybermed_UI/ui.go
+++ cmd/kybermed_UI/ui.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
 
 func main() {
-	a := app.New()
-	mainWindow := a.NewWindow("Login")
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Box Layout")
 
-	mainWindow.SetContent(widget.NewLabel("Hello Wolrd!"))
-	mainWindow.ShowAndRun()
+	text1 := canvas.NewText("Hello", color.White)
+	text2 := canvas.NewText("There", color.White)
+	text3 := canvas.NewText("(right)", color.White)
+	content := container.New(layout.NewHBoxLayout(), text1, text2, layout.NewSpacer(), text3)
+
+	text4 := canvas.NewText("centered", color.White)
+	centered := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), text4, layout.NewSpacer())
+	myWindow.SetContent(container.New(layout.NewVBoxLayout(), content, centered))
+	myWindow.ShowAndRun()
 }
