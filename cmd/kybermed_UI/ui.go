e095755ef6ba1b468176d3e71a3cb4dda87e072e
Nelson Marro <nelsonmarro99@gmail.com>
2025-02-23 09:10:03 -0500

    /workspace/github/nelsonmarro/kyber-med

--- cmd/kybermed_UI/ui.go
+++ cmd/kybermed_UI/ui.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+
+	ui "github.com/nelsonmarro/kyber-med/internal/UI/auth"
 )
 
 func main() {
 	a := app.New()
 	mainWindow := a.NewWindow("Login")
 
-	mainWindow.SetContent(logingContainer)
+	loginPage := ui.NewLoginPage()
+
+	mainWindow.SetContent(loginPage.CreateLoginPage())
 	mainWindow.ShowAndRun()
 }
--- internal/UI/auth/login.go
+++ internal/UI/auth/login.go
@@ -2,14 +2,27 @@ package ui
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 )
 
-func CreateLoginPage() *fyne.Container {
-	loginContainer := container.NewVBox(createPageElements())
-	return l.loginContainer
+type LoginPage struct {
+	loginContainer *fyne.Container
 }
 
-func createPageElements() fyne.CanvasObject {
-	panic("unimplemented")
+func NewLoginPage() *LoginPage {
+	return &LoginPage{}
+}
+
+func (l *LoginPage) CreateLoginPage() *fyne.Container {
+	loginContainer := container.NewVBox(createPageElements()...)
+	return loginContainer
+}
+
+func createPageElements() []fyne.CanvasObject {
+	elemets := []fyne.CanvasObject{
+		canvas.NewImageFromFile("../../../assets/plan.png"),
+	}
+
+	return elemets
 }
