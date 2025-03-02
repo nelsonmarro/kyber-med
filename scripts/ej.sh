e2906a922e94aa4d3dfcea6dc9865ce15bb5b899
Nelson Marro <nelsonmarro99@gmail.com>
2025-03-02 10:40:05 -0500

    /workspace/github/nelsonmarro/kyber-med

---
+++ scripts/ej.sh
@@ -0,0 +1,2 @@
+$ go get github.com/google/uuid // Agrega el paquete google/uuid como dependencia de tu proyecto
+$ go mood tidy // Actualiza el archivo go.mod y go.sum con las dependencias que se encuentran en tu proyecto
